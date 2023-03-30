dcd602246db1e48353754cc031546d4cf0b6740f
D2Cabral <diegomirks@icloud.com>
2023-03-29 21:42:38 -0300

    Refatorando

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,25 +2,17 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo{
     
-    String titulo;
-    String descricao;
-    LocalDate data;
+    
+   private LocalDate data;
 
-    
-    public String getTitulo() {
-        return titulo;
-    }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+   @Override
+   public double calcularXp() {      
+       return XP_PADRAO + 20d;
+   }
+   
+
     public LocalDate getData() {
         return data;
     }
@@ -30,7 +22,7 @@ public class Mentoria {
 
     @Override
     public String toString() {
-        return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", data=" + data + "]";
+        return "Mentoria [titulo=" + getTitulo()+ ", descricao=" + getDescricao() + ", data=" + data + "]";
     }
-    
+   
 }
